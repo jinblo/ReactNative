c3c421db5dda1966b431ec8700145fe672ab61b0
jinblo <katt406@gmail.com>
2024-02-01 18:56:43 +0200

    05 laskin navigoinnilla

--- 01Laskin/App.js
+++ 01Laskin/App.js
@@ -1,71 +1,18 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Button, FlatList, Dimensions } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Calculator from './components/Calculator';
+import History from './components/History';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [result, setResult] = useState(0);
-  const [luku1, setLuku1] = useState(0);
-  const [luku2, setLuku2] = useState(0);
-  const [data, setData] = useState([]);
-
-  const pluslasku = () => {
-    setResult(luku1 + luku2)
-    setData([...data, { key: `${luku1} + ${luku2} = ${luku1 + luku2}` }])
-  }
-
-  const miinuslasku = () => {
-    setResult(luku1 - luku2)
-    setData([...data, { key: `${luku1} - ${luku2} = ${luku1 - luku2}` }])
-  }
-
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-      <TextInput style={styles.input} keyboardType='numeric' onChangeText={text => setLuku1(parseFloat(text))} />
-      <TextInput style={styles.input} keyboardType='numeric' onChangeText={text => setLuku2(parseFloat(text))} />
-      <View style={styles.buttonContainer}>
-        <Button title=" + " onPress={pluslasku} />
-        <Button title=" - " onPress={miinuslasku} />
-      </View>
-      <View style={styles.list}>
-        <Text>History</Text>
-        <FlatList
-          data={data}
-          renderItem={({ item }) =>
-            <Text>{item.key}</Text>
-          }
-          keyExtractor={(item, index) => index.toString()}
-        />
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Calculator" component={Calculator} />
+        <Stack.Screen name="History" component={History} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 100,
-  },
-  input: {
-    width: 100,
-    borderColor: 'gray',
-    borderWidth: 1,
-  },
-  buttonContainer: {
-    flex: 2,
-    width: 100,
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'space-around',
-    padding: 10,
-  },
-  list: {
-    flex: 10,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start'
-  }
-});
